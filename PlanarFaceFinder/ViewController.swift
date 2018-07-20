8806e8d73c392e07de58326ef6751cb1ced09a90
Péter Krassay <peter.krassay@gmail.com>
2018-07-20 23:40:51 +0200

    Initial Commit

---
+++ PlanarFaceFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlanarFaceFinder
+//
+//  Created by Péter Krassay on 2018. 07. 20..
+//  Copyright © 2018. krassay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
